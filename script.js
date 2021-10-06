f628d8bdfaf9bced3546612ac41244a3648ca33a
Sang-ni <dorame1019@gmail.com>
2021-10-06 11:41:51 +0900

    템플릿 리터럴; 플레이스 홀더와 데이터
    플레이스홀더 속 들어가는 내용은 '데이터' → 숫자, 문자, 특수 기호, 상수, 변수, prompt 등
    사용자가 입력한 값을 반환받는 prompt 메소드도 데이터이기 때문에 사용할 수 있다.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let bth = 10.19
 let hername = 'sang.ni';
+const present = prompt("당신이 준비한 선물은?")
 
 console.log("그녀의 이름은 ${hername}, 곧 그녀의 생일 ${bth}");
-console.log(`그녀의 이름은 ${hername}, 곧 그녀의 생일 ${bth}`);
+console.log(
+    `그녀의 이름은 ${'sang.ni'}, 곧 그녀의 생일 ${10.19}이다.
+당신이 준비한 선물은? ${present}`);
