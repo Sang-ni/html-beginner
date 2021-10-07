bf36ef913b87f55be3e7640b9737d49a86c8d404
Sang-ni <dorame1019@gmail.com>
2021-10-07 11:26:40 +0900

    산술 연산
    산술 연산은 더하기, 빼기 등 수를 이용한 계산이다.
    두 개의 숫자 데이터를 피연산자로 받아서 하나의 숫자 데이터를 결과로 반환한다.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let bth = 10.19
-let hername = 'sang.ni';
-const present = prompt("당신이 준비한 선물은?")
-
-console.log("그녀의 이름은 ${hername}, 곧 그녀의 생일 ${bth}");
-console.log(
-    `그녀의 이름은 ${'sang.ni'}, 곧 그녀의 생일 ${10.19}이다.
-당신이 준비한 선물은? ${present}`);
+console.log(7)
+console.log(3+4)
+console.log(3-4)
+console.log(3*4)
+console.log(3/4)
+console.log(3%4)
