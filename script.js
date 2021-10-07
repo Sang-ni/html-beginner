f1f939009be1ec9577519361d3462a44694a500c
Sang-ni <dorame1019@gmail.com>
2021-10-08 07:52:32 +0900

    boolean
    불리언(불린) 데이터: true, false 두 가지 값만 존재.
    참, 거짓 여부를 나타내기 위해 사용
    문자열과는 결과 다르게 출력된다.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let data;
+let data = true; // 참
 console.log(data);
 
-data = "Hello";
+data = false; // 거짓
 console.log(data);
 
-data = null;
+data = "true"; // 문자열
+console.log(data);
+
+data = True; // 소문자로 작성해야 한다.
 console.log(data);
