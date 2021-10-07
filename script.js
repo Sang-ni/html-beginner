14f699623cd6ca15cd92922cd48d90a3a4e2b7b8
Sang-ni <dorame1019@gmail.com>
2021-10-08 07:58:36 +0900

    typeof
    typeof(타입오브): 내가 사용하고 있는 '데이터의 타입이 무엇인지'를 확인해서 반환해주는 연산자.
    출력하고 싶은 데이터 앞에 작성한다.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let data = true; // 참
-console.log(data);
+console.log(typeof data);
 
 data = false; // 거짓
-console.log(data);
+console.log(typeof data);
 
 data = "true"; // 문자열
-console.log(data);
+console.log(typeof data);
 
 data = True; // 소문자로 작성해야 한다.
-console.log(data);
+console.log(typeof data);
