62675bfa2eaa4b11b6e215e2a761c70532a0a65a
Sang-ni <dorame1019@gmail.com>
2021-10-13 18:48:33 +0900

    textContent; 콘텐츠 내용의 변경
    요소가 포함한 텍스트를 읽을 수도, 변경할 수도 있다.
    텍스트 콘텐츠를 표현하는 속성이기에 변경 시, 문자열로 작성
    내용 변경을 먼저 하고 js에 출력하느냐, js에 먼저 출력하고 내용 변경을 하느냐에 따라 콘솔에 출력되는 값이 달라진다.

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const h1 = document.querySelector("h1")
 const p = document.getElementById("text")
 
 console.log(h1.textContent)
+h1.textContent = "안녕 나는 텍스트 콘텐츠"
+
+p.textContent = "겟엘리먼트바이아이디!"
+console.log(p.textContent)
