3ac8520655bc431a56d01ee2c1c2012c3b77a9c0
Sang-ni <dorame1019@gmail.com>
2021-10-13 18:31:00 +0900

    textContent
    해당 노드가 포함하고 있는 텍스트 콘텐츠를 표현하는 속성
    상수나 변수로 지정해준 데이터의 텍스트 콘텐츠를 가져온다.

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-console.log(document.getElementById("text"))
-console.log(document.getElementById("p"))
+const h1 = document.querySelector("h1")
+const p = document.getElementById("text")
+
+console.log(h1.textContent)
