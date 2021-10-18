198b15933d3324618b5bffba969b61a546ff601f
Sang-ni <dorame1019@gmail.com>
2021-10-18 22:42:31 +0900

    의미없는 커밋
    깃허브 기록용

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const p = document.getElementById("text")
 console.log(h1.textContent)
 h1.textContent = "안녕 나는 텍스트 콘텐츠"
 
-p.textContent = "내용 바꾸기!"
+p.textContent = "바쁘다아앙"
 console.log(p.textContent)
